34a3352afe4ac2070ee41165c24bf79e06b9a7dd
OH SEUNGYUN <happy1270111@gmail.com>
2026-02-28 02:20:01 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     <>
     <ReloadPrompt />
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to={localStorage.getItem('accessToken') ? '/home' : '/login'} replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/find-password" element={<FindPassword />} />
